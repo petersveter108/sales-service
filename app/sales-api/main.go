feff0a405720ac60b8985a4dc31d00b01b935893
peter.stevanka <peter.stevanka.it@gmail.com>
2020-12-04 16:06:57 +0100

    Initial commit, basic app with configuration prepared.

---
+++ app/sales-api/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"expvar"
+	"fmt"
+	"github.com/ardanlabs/conf"
+	"github.com/pkg/errors"
+	"log"
+	"os"
+	"time"
+)
+
+/*
+Need to figure out timeouts for http service.
+*/
+
+// build is the git version of this program. It is set using build flags in the makefile.
+var build = "develop"
+
+func main() {
+	log := log.New(os.Stdout, "SALES : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	if err := run(log); err != nil {
+		log.Println("main : error:", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *log.Logger) error {
+
+	// =========================================================================
+	// Configuration
+
+	var cfg struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
+		}
+	}
+	cfg.Version.SVN = build
+	cfg.Version.Desc = "highest truth is highest joy is highest level of functioning (c) yoda"
+
+	if err := conf.Parse(os.Args[1:], "SALES", &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
+
+	// =========================================================================
+	// App Starting
+
+	// Print the build version for our logs. Also expose it under /debug/vars.
+	expvar.NewString("build").Set(build)
+	log.Printf("main : Started : Application initializing : version %q", build)
+	defer log.Println("main : Completed")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "generating config for output")
+	}
+	log.Printf("main : Config :\n%v\n", out)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/petersveter108/sales-service
+
+go 1.15
+
+require (
+	github.com/ardanlabs/conf v1.3.3
+	github.com/pkg/errors v0.9.1
+)
